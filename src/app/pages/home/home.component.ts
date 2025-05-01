0e3ddd9ffded7bb5a9c89a8c6a68c8c27a1ea94d
Raghad Butaiban <raghadbutaiban@gmail.com>
2025-05-01 10:42:09 +0300

    card

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  animations: [
+    trigger("openClose", [
+      state(
+        "active",
+        style({
+          transform: "scale(1) rotateY(0deg)",
+          'box-shadow': '3px 5px 10px rgb(112, 110, 110)'
+        })
+      ),
+      state(
+        "inactive",
+        style({
+          transform: "scale(0.7) rotateY(-180deg)"
+        })
+      ),
+      transition("active <=> inactive", [animate("500ms")])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit{
   userLoggedIn: boolean = false;
   userName: string = "";
+  isCardOpen = false;
 
+  
   constructor(private authService: AuthService, private router: Router){
     
   }
@@ -26,5 +47,10 @@ export class HomeComponent implements OnInit{
       this.userName = this.authService.getLoggedInUser();
     }
   }  
+
+  public toggleCard(): void {
+    this.isCardOpen = !this.isCardOpen;
+  }
+  
     
 }
