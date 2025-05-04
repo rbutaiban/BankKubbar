c953e7d4b68a6e8bdfcbabfba88cd0cf31870e56
AK <aalkhareji27@gmail.com>
2025-05-04 20:03:07 +0300

    Remove NavbarComponent import from AppComponent to streamline dependencies and improve code clarity.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
