6d776fea43c1182d12caba104f5b7ea38e2b1f7d
AK <aalkhareji27@gmail.com>
2025-05-04 22:34:22 +0300

    Update UI components with a cohesive amber color scheme; modify BankCardComponent styles, enhance button component for router link support, and revamp home page layout with new branding and imagery.

--- src/app/components/ui/button/button.component.ts
+++ src/app/components/ui/button/button.component.ts
@@ -1,14 +1,17 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.css',
 })
 export class ButtonComponent {
   @Input() label: string = '';
+  @Input() routerLink: string[] = [];
   @Output() onClick = new EventEmitter<void>();
   @Input() variant: 'fill' | 'outline' | 'text' = 'fill';
 
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,13 @@
 import { Component, inject } from '@angular/core';
-import { TransactionsComponent } from '../transactions/transactions/transactions.component';
-import { BankCardComponent } from '../../components/bank-card/bank-card.component';
-import { UserService } from '../../services/user.service';
-import { AsyncPipe } from '@angular/common';
-import { map } from 'rxjs';
+import { Router } from '@angular/router';
+import { ButtonComponent } from '../../components/ui/button/button.component';
+import { RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent, RouterLinkActive],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {
-}
+export class HomeComponent {}
